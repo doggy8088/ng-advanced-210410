2eb0a7e99d978e8b7432f1f5205f2754487ae74a
Will <doggy.huang@gmail.com>
2021-04-10 11:21:22 +0800

    練習子路由設定 utilities/colors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
   { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'utilities',
+    children: [
+      { path: 'colors', component: ColorsComponent }
+    ]
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
