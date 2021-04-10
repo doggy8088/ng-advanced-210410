9885cc82a304ce62e9de4af1fe429c3a038b370a
Will <doggy.huang@gmail.com>
2021-04-10 14:24:50 +0800

    建立 ComponentsModule 模組（含路由與延遲載入）

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,7 @@ const routes: Routes = [
       },
     ]
   },
+  { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
   { path: '**', component: NotFoundComponent },
 ];
 
