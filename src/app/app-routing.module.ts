e4b505741a79dc613fdb1f537c01189beaf8a0f6
Will <doggy.huang@gmail.com>
2021-04-10 11:11:54 +0800

    示範萬用路由寫法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
   { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
